40b3e79408a135587bcba7a070efe652f82dea25
g18-es <ed_sanchez@live.com>
2017-05-14 23:49:05 -0700

    about page now has profile tabs

--- essm/static/essm_app/js/advice-api.js
+++ essm/static/essm_app/js/advice-api.js
@@ -3,4 +3,10 @@ $(function() {
 	  data = JSON.parse(data);
 	  $( "#advice-section" ).html(data.slip.advice);
 	})
+	$.get( "http://api.adviceslip.com/advice", function( data ) {
+	  data = JSON.parse(data);
+	  $( "#advice-section-2" ).html(data.slip.advice);
+	})
+
+	$('.top.menu .item').tab();
 });
